19c9ec79b64cfc80cbf853e64642b783fa93f4fb
Trịnh Long Vũ <81180330+trinhvu1711@users.noreply.github.com>
2024-03-27 05:12:49 +0700

    data transfer, input validation

---
+++ src/main/java/com/project/shopapp/controller/CategoryController.java
@@ -0,0 +1,49 @@
+package com.project.shopapp.controller;
+
+import com.project.shopapp.dtos.CategoryDTO;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+@RestController
+@RequestMapping("api/v1/categories")
+//@Validated
+public class CategoryController {
+    //    show all category
+    @GetMapping("")//http://localhost:8088/api/v1/categories?page=10&limit=10
+    public ResponseEntity<String> getAllCategory(
+            @RequestParam("page") int page,
+            @RequestParam("limit") int limit) {
+        return ResponseEntity.ok(String.format("get all categories, page = %d, limit = %d", page, limit));
+    }
+
+    @PostMapping("")
+//      Data transfer Object
+    public ResponseEntity<?> insertCategory(
+            @Valid @RequestBody CategoryDTO categoryDTO,
+            BindingResult result) {
+        if (result.hasErrors()){
+            List<String> errorMessages = result.getFieldErrors()
+                    .stream()
+                    .map(FieldError::getDefaultMessage)
+                    .toList();
+            return ResponseEntity.badRequest().body(errorMessages);
+        }
+        return ResponseEntity.ok("This is insertCategory "+categoryDTO);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<String> updateCategory(@PathVariable Long id) {
+        return ResponseEntity.ok("This is updateCategory " + id);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteCategory(@PathVariable Long id) {
+        return ResponseEntity.ok("This is deleteCategory " + id);
+    }
+}
