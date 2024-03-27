8d4f00d8b1e63cc9af3a14364031087a557cc136
Trịnh Long Vũ <81180330+trinhvu1711@users.noreply.github.com>
2024-03-28 04:58:47 +0700

    upload image

--- src/main/java/com/project/shopapp/controller/ProductController.java
+++ src/main/java/com/project/shopapp/controller/ProductController.java
@@ -2,29 +2,82 @@ package com.project.shopapp.controller;
 
 import com.project.shopapp.dtos.ProductDTO;
 import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("api/v1/products")
 public class ProductController {
-    @PostMapping("")
+    @PostMapping(value = "", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
 //      Data transfer Object
     public ResponseEntity<?> insertProduct(
             @Valid @RequestBody ProductDTO productDTO,
+//            @RequestPart("file") MultipartFile file,
             BindingResult result) {
-        if (result.hasErrors()){
-            List<String> errorMessages = result.getFieldErrors()
-                    .stream()
-                    .map(FieldError::getDefaultMessage)
-                    .toList();
-            return ResponseEntity.badRequest().body(errorMessages);
+        try {
+            if (result.hasErrors()){
+                List<String> errorMessages = result.getFieldErrors()
+                        .stream()
+                        .map(FieldError::getDefaultMessage)
+                        .toList();
+                return ResponseEntity.badRequest().body(errorMessages);
+            }
+//        check image file is valid
+            MultipartFile file = productDTO.getFile();
+            if (file != null){
+                if(file.getSize() > 10 * 1024 * 1024){
+                    return ResponseEntity.status(HttpStatus.PAYLOAD_TOO_LARGE).body("File is too large! Maximum size is 1000");
+                }
+                String contentType = file.getContentType();
+                if (contentType == null || !contentType.startsWith("image/")){
+                    return ResponseEntity.status(HttpStatus.UNSUPPORTED_MEDIA_TYPE).body("File must be an image");
+                }
+                String filename = storeFile(file);
+            }
+
+            return ResponseEntity.ok("This is insertProduct "+productDTO);
         }
-        return ResponseEntity.ok("This is insertProduct "+productDTO);
+        catch (Exception e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+
+        }
+    }
+//
+//    {
+//        "name": "ipad pro 2013",
+//            "price": 123.123,
+//            "thumbnail": "this is thumbnail",
+//            "description": "this is description",
+//            "category_id": 1
+//
+//    }
+
+    private String storeFile(MultipartFile file) throws IOException {
+        String filename = StringUtils.cleanPath(file.getOriginalFilename());
+        String uniqueFilename = UUID.randomUUID().toString() + "_" + filename;
+        Path uploadDir = Paths.get("uploads");
+        if (!Files.exists(uploadDir)) {
+            Files.createDirectories(uploadDir);
+        }
+
+        Path destination = Paths.get(uploadDir.toString(), uniqueFilename);
+        Files.copy(file.getInputStream(), destination, StandardCopyOption.REPLACE_EXISTING);
+        return uniqueFilename;
     }
     @GetMapping("")//http://localhost:8088/api/v1/products?page=10&limit=10
     public ResponseEntity<String> getProducts(
