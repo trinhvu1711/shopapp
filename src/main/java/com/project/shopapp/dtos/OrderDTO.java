7b7a1c05f4d7336227a6b03ceea8c9384e58259f
Trịnh Long Vũ <81180330+trinhvu1711@users.noreply.github.com>
2024-04-02 20:52:34 +0700

    create order request

---
+++ src/main/java/com/project/shopapp/dtos/OrderDTO.java
@@ -0,0 +1,36 @@
+package com.project.shopapp.dtos;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderDTO {
+    @JsonProperty("user_id")
+    @Min(value = 1, message = "user's ID must be > 0")
+    private long userId;
+    @JsonProperty("full_name")
+    private String fullName;
+    private String email;
+    @NotBlank(message = "Phone number is required")
+    @Size(min = 5, message = "Phone number must be at least 5 characters")
+    @JsonProperty("phone_number")
+    private String phoneNumber;
+    private String address;
+    private String note;
+    @JsonProperty("total_money")
+    @Min(value = 0, message = "total money must be >= 0")
+    private float totalMoney;
+    @JsonProperty("shipping_method")
+    private String shippingMethod;
+    @JsonProperty("shipping_address")
+    private String shippingAddress;
+    @JsonProperty("payment_method")
+    private String paymentMethod;
+}
