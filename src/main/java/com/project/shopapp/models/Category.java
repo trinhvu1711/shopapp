2204368fbc7d7b784fd06fe70a94c812f130a551
Trịnh Long Vũ <81180330+trinhvu1711@users.noreply.github.com>
2024-04-03 09:56:41 +0700

    create models Product, Category

---
+++ src/main/java/com/project/shopapp/models/Category.java
@@ -0,0 +1,20 @@
+package com.project.shopapp.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name="categories")
+@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "name", nullable = false)
+    private String name;
+
+}
---
+++ src/main/java/com/project/shopapp/models/Product.java
@@ -0,0 +1,25 @@
+package com.project.shopapp.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name="products")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "name", nullable = false, length = 350)
+    private String name;
+    private float price;
+    @Column(name = "thumbnail", length = 300)
+    private String thumbnail;
+    private String description;
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category categoryId;
+}
