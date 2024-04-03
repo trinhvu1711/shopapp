e2dad13cf4514a475592a17a0634af70bf2b15d7
Trịnh Long Vũ <81180330+trinhvu1711@users.noreply.github.com>
2024-04-03 10:28:33 +0700

    create other models
    ProductImage, Role, SocialAccount, Token, User

---
+++ src/main/java/com/project/shopapp/models/SocialAccount.java
@@ -0,0 +1,27 @@
+package com.project.shopapp.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name="social_accounts")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class SocialAccount {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "provider", nullable = false, length = 20)
+    private String provider;
+    @Column(name = "provider_id", length = 50)
+    private String providerId;
+    @Column(name = "name", length = 150)
+    private String name;
+    @Column(name = "email", length = 150)
+    private String email;
+}
---
+++ src/main/java/com/project/shopapp/models/Token.java
@@ -0,0 +1,33 @@
+package com.project.shopapp.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name="tokens")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Token {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "token")
+    private String token;
+    @Column(name = "token_type", length = 50)
+    private String tokenType;
+    @Column(name = "expiration_date")
+    private LocalDateTime expirationDate;
+    private boolean revoked;
+    private boolean expired;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+}
